fad9180965fa4a8348a328087c87565a31832175
Angie Bui <angiebui@gmail.com>
2013-04-24 23:12:26 -0700

    delete unnecessary

--- db/migrate/20130424092957_users.rb
+++
@@ -1,9 +0,0 @@
-class Users < ActiveRecord::Migration
-  def change
-    create_table :urls do |t|
-      t.string :long_url
-      t.string :short_url
-      t.integer :click_count, :default => 0
-    end
-  end
-end
